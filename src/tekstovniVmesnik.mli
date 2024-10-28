b48e3649be2ce9a4c77e308e4584b5635d033ce6
Docker <docker@example.com>
2024-10-28 13:07:48 +0000

    ko izgubis denar lahko ponovno zacnes igro (izboljsano)

--- src/tekstovniVmesnik.mli
+++ src/tekstovniVmesnik.mli
@@ -1,15 +1,15 @@
 (* Funkcija za pridobitev vložka igralca *)
-val ask_for_bet : unit -> unit
+val ask_for_bet : Avtomat.state -> unit
 
 (* Funkcija za prikaz začetnega stanja *)
-val show_initial_state : unit -> unit
+val show_initial_state : Avtomat.state -> unit
 
 (* Funkcija za igro igralca (hit ali stand) *)
-val player_move : unit -> unit
+val player_move : Avtomat.state -> unit
 
 
 (* Funkcija za prikaz rezultata igre *)
-val show_result : unit -> unit
+val show_result : Avtomat.state -> unit
 
 (* Funkcija za zagon igre *)
 val play_game : unit -> unit
